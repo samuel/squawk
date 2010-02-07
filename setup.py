0856a0cbc9e5433259063c99f2991fd0447136c9
Samuel Stauffer <samuel@descolada.com>
2010-02-06 19:52:25 -0800

    Added license to setup.py and LICENSE file (BSD)

--- setup.py
+++ setup.py
@@ -18,10 +18,14 @@ setup(
     author_email = 'samuel@descolada.com',
     url = 'http://github.com/samuel/squawk',
     packages = ['squawk'],
+    license = "BSD",
     classifiers = [
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
